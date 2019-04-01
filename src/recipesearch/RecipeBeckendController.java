d98dbdb2d5e1a7d57ca3bd6f4fdc44c2d18b7e42
jesperlarssson <jesper@jepsoncompedit.se>
2019-04-01 16:43:40 +0200

    Steg 2 + 3

--- src/recipesearch/RecipeBeckendController.java
+++ src/recipesearch/RecipeBeckendController.java
@@ -1,46 +1,42 @@
 package recipesearch;
 
 import se.chalmers.ait.dat215.lab2.Recipe;
+import se.chalmers.ait.dat215.lab2.RecipeDatabase;
+import se.chalmers.ait.dat215.lab2.SearchFilter;
 
 import java.util.List;
 
 public class RecipeBeckendController {
 
-
-
-
-
+    private RecipeDatabase db = RecipeDatabase.getSharedInstance();
     private String cuisine;
     private String mainIngredient;
     private String difficulty;
     private int maxPrice;
     private int maxTime;
 
-
-
-
     public List<Recipe> getRecipes(){
-        return null;
+        return db.search(new SearchFilter(difficulty,maxTime,cuisine,maxPrice,mainIngredient));
     }
 
     public void setCuisine(String cuisine){
-
+        this.cuisine = cuisine;
     }
 
     public void setMainIngredient(String mainIngredient){
-
+        this.mainIngredient = mainIngredient;
     }
 
     public void setDifficulty(String difficulty){
-
+        this.difficulty = difficulty;
     }
 
     public void setMaxPrice(int maxPrice){
-
+        this.maxPrice = maxPrice;
     }
 
     public void setMaxTime(int maxTime){
-
+        this.maxTime = maxTime;
     }
 
 
