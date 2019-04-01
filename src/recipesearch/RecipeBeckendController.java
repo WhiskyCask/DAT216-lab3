287caea61f2183bbcc907ee02ca65b9d441e8ba7
jesperlarssson <44870997+jesperlarssson@users.noreply.github.com>
2019-04-01 15:59:53 +0200

    Add files via upload

---
+++ src/recipesearch/RecipeBeckendController.java
@@ -0,0 +1,47 @@
+package recipesearch;
+
+import se.chalmers.ait.dat215.lab2.Recipe;
+
+import java.util.List;
+
+public class RecipeBeckendController {
+
+
+
+
+
+    private String cuisine;
+    private String mainIngredient;
+    private String difficulty;
+    private int maxPrice;
+    private int maxTime;
+
+
+
+
+    public List<Recipe> getRecipes(){
+        return null;
+    }
+
+    public void setCuisine(String cuisine){
+
+    }
+
+    public void setMainIngredient(String mainIngredient){
+
+    }
+
+    public void setDifficulty(String difficulty){
+
+    }
+
+    public void setMaxPrice(int maxPrice){
+
+    }
+
+    public void setMaxTime(int maxTime){
+
+    }
+
+
+}
