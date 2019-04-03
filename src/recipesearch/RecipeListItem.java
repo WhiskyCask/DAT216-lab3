7b48594c1793975f987ab9f58e2a54921683583d
jesperlarssson <jesper@jepsoncompedit.se>
2019-04-03 15:43:05 +0200

    add fxml listitem

--- src/recipesearch/RecipeListItem.java
+++ src/recipesearch/RecipeListItem.java
@@ -1,8 +1,11 @@
 package recipesearch;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 import se.chalmers.ait.dat215.lab2.Recipe;
 
 import java.io.IOException;
@@ -10,6 +13,8 @@ import java.io.IOException;
 public class RecipeListItem extends AnchorPane {
     private RecipeSearchController parentController;
     private Recipe recipe;
+    @FXML private ImageView imgItemList;
+    @FXML private Text descriptionItemList;
 
     public RecipeListItem(Recipe recipe, RecipeSearchController recipeSearchController){
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("recipe_listitem.fxml"));
