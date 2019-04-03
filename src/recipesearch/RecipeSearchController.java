a7122d6b09ee7e48b7454c34a6a73f540ea8d8f3
WhiskyCask <systembolagetag@gmail.com>
2019-04-03 15:20:38 +0200

    Variabler

--- src/recipesearch/RecipeSearchController.java
+++ src/recipesearch/RecipeSearchController.java
@@ -3,14 +3,30 @@ package recipesearch;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
 import se.chalmers.ait.dat215.lab2.RecipeDatabase;
 
 
 public class RecipeSearchController implements Initializable {
 
     RecipeDatabase db = RecipeDatabase.getSharedInstance();
-    
+
+    @FXML private ComboBox mainIngredientComboBox;
+    @FXML private ComboBox cuisineComboBox;
+    @FXML private RadioButton radioButtonAll;
+    @FXML private RadioButton radioButtonEasy;
+    @FXML private RadioButton radioButtonMedium;
+    @FXML private RadioButton radioButtonHard;
+    @FXML private Spinner maxPriceSpinner;
+    @FXML private Slider maxTimeSlider;
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
